46b961acbde93b747d68e9eb2365f1ef64e4dedc
andre monteiro <124000838+AndreMonteiro225@users.noreply.github.com>
2025-12-10 13:10:26 -0300

    update imgs

--- src/app/components/Header/Header.jsx
+++ src/app/components/Header/Header.jsx
@@ -1,20 +1,23 @@
-'use client';
-import { useState } from 'react';
-import styles from './Header.module.css'; 
-import Link from 'next/link';
+"use client";
+import { useState } from "react";
+import styles from "./Header.module.css";
+import Link from "next/link";
+import Image from "next/image";
 
 export default function Header() {
-  const phone = '+5511970680610';
+  const phone = "+5511970680610";
 
   const [isOpen, setIsOpen] = useState(false);
 
   const toggleMenu = () => setIsOpen(!isOpen);
-  
+
   return (
     <>
       <header className={styles.head}>
         <div className={styles.logo}>
-          <Link href="/"><i className="fas fa-layer-group"></i> Topoleasing</Link>
+          <Link href="/">
+            <Image width={170} height={80} src={"/Gemini_Generated_Image_4468hj4468hj4468.webp"} />
+          </Link>
         </div>
 
         {/* Desktop Menu */}
@@ -32,22 +35,24 @@ export default function Header() {
       </header>
 
       {/* Mobile Overlay & Menu */}
-      {isOpen && (
-        <div className={styles.overlay} onClick={toggleMenu}></div>
-      )}
-      
-      <nav className={`${styles.mobileModal} ${isOpen ? styles.active : ''}`}>
+      {isOpen && <div className={styles.overlay} onClick={toggleMenu}></div>}
+
+      <nav className={`${styles.mobileModal} ${isOpen ? styles.active : ""}`}>
         <div className={styles.closeModal} onClick={toggleMenu}>
           <i className="fas fa-times"></i>
         </div>
-        <h3 style={{ color: 'var(--primary-blue)' }}>Menu Rápido</h3>
+        <h3 style={{ color: "var(--primary-blue)" }}>Menu Rápido</h3>
         <a href={`https://wa.me/${phone}`} className={styles.navBtn}>
           <i className="fab fa-whatsapp"></i> WhatsApp
         </a>
         <Link href="#contact" className={styles.navBtn} onClick={toggleMenu}>
           <i className="fas fa-envelope"></i> E-mail
         </Link>
-        <Link href="/CatalogPage" className={styles.navBtn} onClick={toggleMenu}>
+        <Link
+          href="/CatalogPage"
+          className={styles.navBtn}
+          onClick={toggleMenu}
+        >
           <i className="fas fa-layer-group"></i> Equipamentos
         </Link>
         <Link href="#services" className={styles.navBtn} onClick={toggleMenu}>
